7b4406bdae4876672fb2736da428cb7256a83d67
Rennie Bevineau <rsbevineau@gmail.com>
2020-10-03 23:18:17 -0400

    change name of the database

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/dance',
+    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/total_dance',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 
 }
