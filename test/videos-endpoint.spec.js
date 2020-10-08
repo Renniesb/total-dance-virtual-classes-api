a8591197a4c0e2d7ad72641f37a3aa56a455e336
Rennie Bevineau <rsbevineau@gmail.com>
2020-10-08 00:50:10 -0400

    add test to get specific videos

---
+++ test/videos-endpoint.spec.js
@@ -0,0 +1,49 @@
+const knex = require('knex')
+const app = require('../app')
+const makeVideosArray = require('./videos.fixtures.js')
+
+
+describe('test getting dance videos', function() {
+    let db
+    
+    const testVideos = makeVideosArray()
+    
+    before('make knex instance', () => {
+      db = knex({
+        client: 'pg',
+        connection: process.env.TEST_DB_URL,
+      })
+      app.set('db', db)
+    })
+  
+    after('disconnect from db', () => db.destroy())
+  
+    before('cleanup', () => db.raw('TRUNCATE TABLE videos'))
+  
+    afterEach('cleanup', () => db.raw('TRUNCATE TABLE videos'))
+  
+    describe(`GET /api/videos/:dancetype/:dancelevel`, () => {
+      beforeEach('insert videos', () => {
+        return db
+            .into('videos')
+            .insert(testVideos)
+      })
+  
+      it('responds with 200 and all of the questions', () => {
+        const dancetype = 'Salsa'
+        const level = 'intermediate'
+        let testfilteredVideos = testVideos.filter(video => video.dancetype == dancetype && video.dancelevel == level)
+        return supertest(app)
+          .get(`/api/videos/${dancetype}/${level}`)
+          .expect(200, testfilteredVideos)
+      })
+  
+      
+  
+      
+    })
+  })
+
+  module.exports = {
+      makeVideosArray
+  }
--- test/videos.fixtures.js
+++ test/videos.fixtures.js
@@ -1,20 +1,15 @@
-function makeQuizArray() {
+function makeVideosArray() {
     return [
-        {
-            id: 1,
-            "quizname":"Conversational Quiz",
-            "quizdescription":"A quiz to test your conversational comprehension",
-            "level":"Intermediate",
-            "locked":true
-          },
-          {
-            id: 2,
-            "quizname":"Video Quiz",
-            "quizdescription":"A Video quiz to test your conversational comprehension",
-            "level":"Beginner",
-            "locked":false
-          },
+      {id: 1 , dancetype:'Salsa',dancelevel:'beginner',movetitle: 'How To Spin Your Partner',link: '46dybRGrCsc'},
+      {id: 2 , dancetype:'Salsa',dancelevel:'beginner',movetitle: 'Make your basic step look more natural',link: '9CzGaubcqQg'},
+      {id: 3 , dancetype:'Salsa',dancelevel:'beginner', movetitle: 'Closed position lead setup' ,link: 'vWeJbJgkTFQ'},
+      {id: 4 , dancetype:'Salsa',dancelevel:'intermediate', movetitle: 'Pencil turns' ,link: 'ADysVrHMp3g'},
+      {id: 5 , dancetype:'Salsa',dancelevel:'intermediate', movetitle: 'Lady double turn' ,link: 'TDoyttBpzU0'},
+      {id: 6 , dancetype:'Salsa',dancelevel:'intermediate', movetitle: 'Underarm turns' ,link: 'lhu6spVILW8'},
+      {id: 7 , dancetype:'Salsa',dancelevel:'advanced', movetitle: 'Back spot turn into cross body lead' ,link: 'ln9nxU-xN6A'},
+      {id: 8 , dancetype:'Salsa',dancelevel:'advanced', movetitle: 'Breakaway turn (inspired by Lindy hop)' ,link: 'ziH6X4QHITY'},
+      {id: 9 , dancetype:'Salsa',dancelevel:'advanced', movetitle: 'Back spot turn into titanic' ,link: 'yPkNaOGyAyc'}
     ]
 }
 
-module.exports =  makeQuizArray
+module.exports =  makeVideosArray
