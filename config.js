4c1a56d3debc51fafb2ab8c32ac72c17ffbaf0ce
Rennie Bevineau <rsbevineau@gmail.com>
2020-10-08 21:48:38 -0400

    replace DB_URL with DATABASE_URL for Heroku

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/total_dance',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/total_dance',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    TEST_DB_URL: process.env.TEST_DB_URL  || 'postgresql://postgres@localhost/total_dance'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL  || 'postgresql://postgres@localhost/total_dance'
 
 }
